265ea423dfff5d1165738a79ce351d202cafabef
Yamilex Avila-Stanley <yavila.stanley@gmail.com>
2019-04-21 12:45:58 -0700

    add apollo.config.js

---
+++ packages/frontend/apollo.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    client: {
+        service: "blasters" // the id of your service in Engine (from the URL)
+    },
+    service: {
+      endpoint: {
+        url: "http://localhost:4000"
+      }
+    }
+  };
