065ea07c2a2593cafa924c529873192c3a253ce3
Lucas Della Bella <dellabella.lucas@gmail.com>
2019-04-27 23:06:34 -0700

    Move codesandbox all to one file

--- packages/frontend/src/components/App.tsx
+++ packages/frontend/src/components/App.tsx
@@ -5,15 +5,17 @@ import ActivityFeed from './ActivityFeed';
 import CreateProject from './CreateProject';
 import Header from './Header';
 import LoginSignupForm from './LoginSignupForm';
+import ProfilePage from './ProfilePage';
 
 const App: React.FunctionComponent = () => (
   <div>
     <Header />
     <div>
       <Switch>
+        <Route exact path="/" component={ActivityFeed} />
         <Route exact path="/login" component={LoginSignupForm} />
         <Route exact path="/create" component={CreateProject} />
-        <Route exact path="/" component={ActivityFeed} />
+        <Route exact path="/profile" component={ProfilePage} />
       </Switch>
     </div>
   </div>
---
+++ packages/frontend/src/components/ProfilePage.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Project } from '../generated/graphql';
+
+import '../styles/ProfilePage.css';
+
+const Ribbon = () => (
+  <div className="ribbon">
+    <div className="triangle" />
+  </div>
+);
+
+const Ribbons = () => (
+  <div className="ribbon-container">
+    <Ribbon />
+    <Ribbon />
+    <Ribbon />
+    <Ribbon />
+  </div>
+);
+
+interface FeaturedProjectProps {
+  imageUrl: string;
+}
+
+const FeaturedProject = ({ imageUrl }: FeaturedProjectProps) => (
+  <div className="featured-project">
+    <img src={imageUrl} />
+  </div>
+);
+
+interface OtherProjectsProps {
+  projects: [Project];
+}
+
+const OtherProjects = ({ projects }: OtherProjectsProps) => (
+  <div className="other-content">
+    {projects.map(project => (
+      <div>
+        <img src={project.imageUrl} />
+      </div>
+    ))}
+  </div>
+);
+
+const otherProjects = [
+  { imageUrl: 'https://i.imgur.com/aBQQMc6.png' },
+  { imageUrl: 'https://i.imgur.com/cxff7sO.png' },
+  { imageUrl: 'https://i.imgur.com/aBQQMc6.png' },
+  { imageUrl: 'https://i.imgur.com/cxff7sO.png' },
+  { imageUrl: 'https://i.imgur.com/aBQQMc6.png' },
+  { imageUrl: 'https://i.imgur.com/cxff7sO.png' },
+];
+
+const ProfilePage = () => {
+  return (
+    <div className="Profile">
+      <div className="cover-photo" />
+      <div className="profile-card">
+        <Ribbons />
+        <div className="profile-image">
+          <img src="https://i.imgur.com/aBQQMc6.png" />
+        </div>
+      </div>
+      <div className="project-container">
+        <FeaturedProject imageUrl={'https://i.imgur.com/aBQQMc6.png'} />
+        <OtherProjects projects={otherProjects} />
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
