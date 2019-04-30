ccf5681c42cf4e9f47835b3eebc2b40532791c7f
Lucas Della Bella <dellabella.lucas@gmail.com>
2019-04-29 22:34:50 -0700

    Put login and signup inside modal, backdrop click to dismiss not working

--- packages/frontend/src/components/App.tsx
+++ packages/frontend/src/components/App.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import '../styles/App.css';
 import ActivityFeed from './ActivityFeed';
@@ -6,19 +6,26 @@ import CreateProject from './CreateProject';
 import Header from './Header';
 import LoginSignupForm from './LoginSignupForm';
 import ProfilePage from './ProfilePage';
+import Modal from '@material-ui/core/Modal';
 
-const App: React.FunctionComponent = () => (
-  <div>
-    <Header />
+const App: React.FunctionComponent = () => {
+  const [isLoginSignupModalOpen, setLoginSignupModalOpen] = useState(false);
+  return (
     <div>
-      <Switch>
-        <Route exact path="/" component={ActivityFeed} />
-        <Route exact path="/login" component={LoginSignupForm} />
-        <Route exact path="/create" component={CreateProject} />
-        <Route exact path="/profile" component={ProfilePage} />
-      </Switch>
+      <Header openLoginSignupModal={() => setLoginSignupModalOpen(true)} />
+      <Modal open={isLoginSignupModalOpen}>
+        <LoginSignupForm />
+      </Modal>
+      <div>
+        <Switch>
+          <Route exact path="/" component={ActivityFeed} />
+          <Route exact path="/login" component={LoginSignupForm} />
+          <Route exact path="/create" component={CreateProject} />
+          <Route exact path="/profile" component={ProfilePage} />
+        </Switch>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
--- packages/frontend/src/components/LoginSignupForm.tsx
+++ packages/frontend/src/components/LoginSignupForm.tsx
@@ -6,6 +6,8 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
+import '../styles/App.css';
+
 const LoginSignupForm = () => {
   const [state, setState] = useState({
     email: '',
@@ -17,62 +19,59 @@ const LoginSignupForm = () => {
   const { login, email, password, name } = state;
 
   return (
-    <div>
-      <Paper elevation={1}>
-        <Typography variant="h4" component="h3">
-          {login ? 'Login' : 'Sign Up'}
-        </Typography>
-        <form className="flex flex-column">
-          {!login && <TextField label="Name" margin="normal" />}
-          <TextField
-            label="Email Address"
-            type="text"
-            placeholder="Your email address"
-          />
-          <TextField label="Password" type="text" placeholder="Your password" />
-          <TextField
-            label="Password Confirmation"
-            type="text"
-            placeholder="Your password"
-          />
-        </form>
-        <div className="flex mt3">
-          {login ? (
-            <Login variables={{ email, password }}>
-              {loginMutation => (
-                <Button
-                  className="pointer mr2 button"
-                  variant="contained"
-                  color="primary"
-                  onClick={() => loginMutation()}
-                >
-                  Login
-                </Button>
-              )}
-            </Login>
-          ) : (
-            <Signup variables={{ email, password, name }}>
-              {signupMutation => (
-                <Button
-                  className="pointer mr2 button"
-                  variant="contained"
-                  color="secondary"
-                  onClick={() => signupMutation()}
-                >
-                  Signup
-                </Button>
-              )}
-            </Signup>
-          )}
-          <div
-            className="pointer button"
-            onClick={() => setState({ ...state, login: !login })}
-          >
-            {login ? 'need to create an account?' : 'already have an account?'}
-          </div>
-        </div>
-      </Paper>
-    </div>
+    <Paper className="login-signup-modal" elevation={1}>
+      <Typography variant="h4" component="h3">
+        {login ? 'Login' : 'Sign Up'}
+      </Typography>
+      <form className="flex flex-column">
+        {!login && <TextField label="Name" margin="normal" />}
+        <TextField
+          label="Email Address"
+          type="text"
+          placeholder="Your email address"
+        />
+        <TextField label="Password" type="text" placeholder="Your password" />
+        <TextField
+          label="Password Confirmation"
+          type="text"
+          placeholder="Your password"
+        />
+      </form>
+      <div className="flex mt3">
+        {login ? (
+          <Login variables={{ email, password }}>
+            {loginMutation => (
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => loginMutation()}
+              >
+                Login
+              </Button>
+            )}
+          </Login>
+        ) : (
+          <Signup variables={{ email, password, name }}>
+            {signupMutation => (
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => signupMutation()}
+              >
+                Signup
+              </Button>
+            )}
+          </Signup>
+        )}
+        <Button
+          color="secondary"
+          variant="outlined"
+          onClick={() => setState({ ...state, login: !login })}
+        >
+          {login ? 'need to create an account?' : 'already have an account?'}
+        </Button>
+      </div>
+    </Paper>
   );
 };
 
